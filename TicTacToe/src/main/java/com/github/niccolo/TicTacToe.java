4af08f50b6d3e6f28453aacc71304dc84455a728
Niccolò Alt <niccoloj.alt@gmail.com>
2025-11-02 22:46:31 +0100

    rivincita, colori

--- TicTacToe/src/main/java/com/github/niccolo/TicTacToe.java
+++ TicTacToe/src/main/java/com/github/niccolo/TicTacToe.java
@@ -7,24 +7,24 @@ import javax.swing.*;
 import java.io.InputStream;
 
 public class TicTacToe implements ActionListener {
-
     Random random = new Random();
     JFrame frame = new JFrame("Tris");
     JPanel pannelloTitolo = new JPanel();
     JPanel pannelloBottoni = new JPanel();
     JLabel casellaTesto = new JLabel();
     JButton[] caselle = new JButton[9];
-    boolean turnoPlayer1;
+    boolean player1_turn;
+    JButton nuovaPartita = new JButton("Nuova Partita");
+    boolean giocoFinito = false;
 
-    public TicTacToe() {
-
+    TicTacToe() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600, 600);
         frame.setLayout(new BorderLayout());
         frame.getContentPane().setBackground(Color.BLACK);
 
         try {
-            java.net.URL iconURL = getClass().getResource("/photos/icon.png");
+            java.net.URL iconURL = getClass().getResource("/photos/iconaTris.png");
             if (iconURL == null) throw new Exception("Icona non trovata nel classpath!");
             Image icon = Toolkit.getDefaultToolkit().getImage(iconURL);
             frame.setIconImage(icon);
@@ -32,11 +32,10 @@ public class TicTacToe implements ActionListener {
             System.out.println("Impossibile caricare l'icona personalizzata.");
         }
 
-        casellaTesto.setBackground(Color.LIGHT_GRAY);
+        casellaTesto.setBackground(Color.BLACK);
         casellaTesto.setForeground(Color.WHITE);
         casellaTesto.setHorizontalAlignment(JLabel.CENTER);
         casellaTesto.setOpaque(true);
-
         try (InputStream fontStream = getClass().getResourceAsStream("/fonts/ProstoOne-Regular.ttf")) {
             if (fontStream == null) throw new Exception("Font non trovato nel classpath!");
             Font prostoOne = Font.createFont(Font.TRUETYPE_FONT, fontStream).deriveFont(Font.BOLD, 30f);
@@ -45,10 +44,10 @@ public class TicTacToe implements ActionListener {
             e.printStackTrace();
             casellaTesto.setFont(new Font("SansSerif", Font.BOLD, 30));
         }
-
         casellaTesto.setText("Tris");
+
         pannelloTitolo.setLayout(new BorderLayout());
-        pannelloTitolo.add(casellaTesto);
+        pannelloTitolo.add(casellaTesto, BorderLayout.CENTER);
         frame.add(pannelloTitolo, BorderLayout.NORTH);
 
         pannelloBottoni.setLayout(new GridLayout(3, 3));
@@ -67,28 +66,39 @@ public class TicTacToe implements ActionListener {
             caselle[i] = new JButton();
             caselle[i].setFont(fontXO);
             caselle[i].setFocusable(false);
+            caselle[i].setBackground(new Color(117, 241, 231));
+            caselle[i].setForeground(Color.DARK_GRAY);
             caselle[i].addActionListener(this);
             pannelloBottoni.add(caselle[i]);
         }
-
         frame.add(pannelloBottoni, BorderLayout.CENTER);
+
+        nuovaPartita.setVisible(false);
+        nuovaPartita.setFocusable(false);
+        nuovaPartita.setFont(new Font("SansSerif", Font.BOLD, 30)); // più grande
+        nuovaPartita.addActionListener(e -> reset());
+        frame.add(nuovaPartita, BorderLayout.SOUTH);
+
         frame.setVisible(true);
+
         primoTurno();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (giocoFinito) return;
+
         for (int i = 0; i < 9; i++) {
             if (e.getSource() == caselle[i] && caselle[i].getText().equals("")) {
-                if (turnoPlayer1) {
-                    caselle[i].setForeground(Color.YELLOW);
+                if (player1_turn) {
+                    caselle[i].setForeground(Color.DARK_GRAY);
                     caselle[i].setText("X");
-                    turnoPlayer1 = false;
+                    player1_turn = false;
                     casellaTesto.setText("Tocca a O");
                 } else {
-                    caselle[i].setForeground(Color.GREEN);
+                    caselle[i].setForeground(Color.WHITE);
                     caselle[i].setText("O");
-                    turnoPlayer1 = true;
+                    player1_turn = true;
                     casellaTesto.setText("Tocca a X");
                 }
                 controlla();
@@ -97,50 +107,103 @@ public class TicTacToe implements ActionListener {
     }
 
     public void primoTurno() {
-        try {
-            Thread.sleep(400);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         if (random.nextInt(2) == 0) {
-            turnoPlayer1 = true;
+            player1_turn = true;
             casellaTesto.setText("Tocca a X");
         } else {
-            turnoPlayer1 = false;
+            player1_turn = false;
             casellaTesto.setText("Tocca a O");
         }
     }
 
     public void controlla() {
         int[][] combinazioni = {
-                {0,1,2}, {3,4,5}, {6,7,8},
-                {0,3,6}, {1,4,7}, {2,5,8},
-                {0,4,8}, {2,4,6}
+                {0,1,2}, {3,4,5}, {6,7,8}, // righe
+                {0,3,6}, {1,4,7}, {2,5,8}, // colonne
+                {0,4,8}, {2,4,6}           // diagonali
         };
+
+        boolean vincitore = false;
+
         for (int[] combo : combinazioni) {
             String a = caselle[combo[0]].getText();
             String b = caselle[combo[1]].getText();
             String c = caselle[combo[2]].getText();
+
             if (!a.equals("") && a.equals(b) && b.equals(c)) {
+                vincitore = true;
                 if (a.equals("X")) xVince(combo[0], combo[1], combo[2]);
                 else oVince(combo[0], combo[1], combo[2]);
             }
         }
+
+        boolean pieno = true;
+        for (JButton btn : caselle) {
+            if (btn.getText().equals("")) {
+                pieno = false;
+                break;
+            }
+        }
+
+        if (!vincitore && pieno) {
+            giocoFinito = true;
+            casellaTesto.setText("Pareggio!");
+            for (JButton btn : caselle) btn.setEnabled(false);
+            nuovaPartita.setVisible(true);
+        }
     }
 
     public void xVince(int a, int b, int c) {
-        caselle[a].setBackground(Color.PINK);
-        caselle[b].setBackground(Color.PINK);
-        caselle[c].setBackground(Color.PINK);
+        giocoFinito = true;
+        caselle[a].setForeground(Color.BLACK);
+        caselle[b].setForeground(Color.BLACK);
+        caselle[c].setForeground(Color.BLACK);
+
+        for (int i = 0; i < caselle.length; i++) {
+            if (i != a && i != b && i != c) {
+                caselle[i].setForeground(Color.DARK_GRAY);
+            }
+        }
+
+        caselle[a].setBackground(Color.GREEN);
+        caselle[b].setBackground(Color.GREEN);
+        caselle[c].setBackground(Color.GREEN);
+
         for (JButton btn : caselle) btn.setEnabled(false);
-        casellaTesto.setText("X wins!");
+        casellaTesto.setText("X vince!");
+        nuovaPartita.setVisible(true);
     }
 
     public void oVince(int a, int b, int c) {
-        caselle[a].setBackground(Color.ORANGE);
-        caselle[b].setBackground(Color.ORANGE);
-        caselle[c].setBackground(Color.ORANGE);
+        giocoFinito = true;
+        caselle[a].setForeground(Color.BLACK);
+        caselle[b].setForeground(Color.BLACK);
+        caselle[c].setForeground(Color.BLACK);
+
+        for (int i = 0; i < caselle.length; i++) {
+            if (i != a && i != b && i != c) {
+                caselle[i].setForeground(Color.DARK_GRAY);
+            }
+        }
+
+        caselle[a].setBackground(Color.GREEN);
+        caselle[b].setBackground(Color.GREEN);
+        caselle[c].setBackground(Color.GREEN);
+
         for (JButton btn : caselle) btn.setEnabled(false);
-        casellaTesto.setText("O wins!");
+        casellaTesto.setText("O vince!");
+        nuovaPartita.setVisible(true);
+    }
+
+    public void reset() {
+        giocoFinito = false;
+        for (JButton btn : caselle) {
+            btn.setText("");
+            btn.setEnabled(true);
+            btn.setBackground(new Color(117, 241, 231));
+            btn.setForeground(Color.DARK_GRAY);
+        }
+        nuovaPartita.setVisible(false);
+        primoTurno();
     }
 }
